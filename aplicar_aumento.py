ee2b13c13f8fec2ed95200e149993cf1fa69d5f3
Norberto Diman Junior <norbertodimanjunior2026@gmail.com>
2026-03-03 19:23:56 -0300

    initial commit

---
+++ aplicar_aumento.py
@@ -0,0 +1,4 @@
+valor = float(input("Digite o valor que deseja aplicar o aumento de 10%: "))
+aplicar_aumento = valor * 1.10
+
+print(f"O valor {valor:.2f} com aumento de 10% é {aplicar_aumento:.2f}")
